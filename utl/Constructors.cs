337cfce83e18a56dc94d626d4c09c6453a358347
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-02-18 18:07:55 -0300

    Add class Constructors empty

---
+++ utl/Constructors.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Channels;
+
+namespace dot.utl
+{
+    public class Constructors
+    {
+        public Constructors()
+        {
+            Console.WriteLine("Pass...");
+        }
+    }
+}
