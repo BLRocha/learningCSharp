64da9dd9528cfb44760ab78a8d77dc27824aaafb
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-02-24 17:58:08 -0300

    add enumerate

---
+++ enumComposicao/OrderStatus.cs
@@ -0,0 +1,10 @@
+namespace dot.enumComposicao
+{
+    public enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
