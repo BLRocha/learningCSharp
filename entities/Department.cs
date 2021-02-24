17bb801ade2c333975272ba89a61b2a5d7f8e8c4
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-02-24 20:42:11 -0300

    add class name worker department

---
+++ entities/Department.cs
@@ -0,0 +1,16 @@
+namespace dot.entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
