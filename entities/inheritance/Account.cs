f671c13707c0bc53f37ecd3ad879505b609daa56
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-02-25 18:34:18 -0300

    Add class Account attr and methods

---
+++ entities/inheritance/Account.cs
@@ -0,0 +1,41 @@
+using System.Text;
+
+namespace dot.entities.inheritance
+{
+    public class Account
+    {
+        public uint  Number { get; protected set; }
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
+        
+        public Account(uint number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Account Number: {Number}");
+            sb.AppendLine($"Holder: {Holder}");
+            sb.AppendLine($"Balance U$ {Balance}");
+            return sb.ToString();
+        }
+    }
+}
