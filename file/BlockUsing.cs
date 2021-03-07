215e1ade18eb6362dc5509d39874066b250fc436
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-03-07 02:21:05 -0300

    add class BlockUsing auto close files

---
+++ file/BlockUsing.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+
+namespace dot.file
+{
+    public class BlockUsing
+    {
+        private string _path = @"../../../file/lorem.txt";
+
+        public void UsingTest()
+        {
+            using (FileStream fs = new FileStream(_path, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    int count = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine($"Line number: {count}, Content: {sr.ReadLine()}");
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
